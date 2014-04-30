9a639c3d7e011420b3e2568eabe6aed38334205d
Francisco José Marques Vieira <francisco.vieira@co.sapo.pt>
2014-04-30 17:27:34 +0100

    Improve __init__.py file

--- securitylib/__init__.py
+++ securitylib/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['crypto', 'random', 'passwords']
+__all__ = ['crypto', 'advanced_crypto', 'random', 'passwords', 'throttling']
+
+import crypto
+import advanced_crypto
+import random
+import passwords
+import throttling
