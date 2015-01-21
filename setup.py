ce87d5830cd92729e461a567062e67e1dbeab08f
Hugo Castilho <hugo.p.castilho@telecom.pt>
2015-01-21 18:37:25 +0000

    Changes to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -13,19 +13,29 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'pycrypto',
+        'pycrypto>=2.6.1',
     ],
     package_data={
         '': ['*.txt', '*.md'],
     },
 
-    author='Francisco José Marques Vieira',
-    author_email='francisco.vieira@co.sapo.pt',
-    url='http://trac.intra.sapo.pt/securitylib/',
-    description=('The SAPO Security Lib is a library whose purpose is to provide functions/classes '
-        'that solve common security related problems, while being easy to use even by those who are not security experts.'),
+    author='Hugo Peres Castilho',
+    author_email='hugo.p.castilho@telecom.pt',
+    url='http://oss.sapo.pt/securitylib-python/',
+    description=('The SAPO Security Lib is a library whose purpose is to '
+        'provide functions/classes that solve common security related problems,'
+        ' while being easy to use even by those who are not security experts.'),
     long_description = get_readme(),
-    license='GPL',
-    keywords='',
-    classifiers=[],
+    license='MIT',
+    keywords=['security', 'crypto', 'securitylib'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2',
+        'Topic :: Security :: Cryptography',
+        ],
 )
